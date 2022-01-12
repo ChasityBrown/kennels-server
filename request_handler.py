35a0cd650db1345cf61105a5d3f4187ddf909d77
ChasityBrown <chasity.brown@asurion.com>
2022-01-12 15:23:46 -0500

    Chapter 6 updating dictionaries

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
-from views import get_all_locations, get_single_location, create_location, delete_location
-from views import get_all_employees, get_single_employee, create_employee, delete_employee
-from views import get_all_customers, get_single_customer, create_customer, delete_customer
+from views import get_all_locations, get_single_location, create_location, delete_location, update_location
+from views import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
+from views import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
 from .animal_requests import delete_animal, get_all_animals, get_single_animal, create_animal, update_animal
-from .locations_requests import delete_location, get_all_locations, get_single_location, create_location
-from .employees_requests import delete_employee, get_all_employees, get_single_employee, create_employee
-from .customers_requests import get_all_customers, get_single_customer, create_customer, delete_customer
+from .locations_requests import delete_location, get_all_locations, get_single_location, create_location, update_location
+from .employees_requests import delete_employee, get_all_employees, get_single_employee, create_employee, update_employee
+from .customers_requests import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
