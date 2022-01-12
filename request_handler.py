76e01df33f4315f6bba6f1a6133bd1f58ea47d04
ChasityBrown <chasity.brown@asurion.com>
2022-01-12 15:12:29 -0500

    Chapter 5

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,9 @@
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals
+from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from views import get_all_locations, get_single_location, create_location, delete_location
+from views import get_all_employees, get_single_employee, create_employee, delete_employee
+from views import get_all_customers, get_single_customer, create_customer, delete_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -13,7 +17,28 @@ class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
 
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
     # Here's a class function
+
     def _set_headers(self, status):
         # Notice this Docstring also includes information about the arguments passed to the function
         """Sets the status code, Content-Type and Access-Control-Allow-Origin
@@ -46,31 +71,136 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         # Set the response code to 'Ok'
         self._set_headers(200)
+        response = {}  # Default response
 
-        # Your new console.log() that outputs to the terminal
-        print(self.path)
+       # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
 
         # It's an if..else statement
-        if self.path == "/animals":
-            response = get_all_animals()
-        else:
-            response = []
+        if resource == "animals":
+            if id is not None:
+                response = f"{get_single_animal(id)}"
+
+            else:
+                response = f"{get_all_animals()}"
+
+        if resource == "locations":
+            if id is not None:
+                response = f"{get_single_location(id)}"
+
+            else:
+                response = f"{get_all_locations()}"
+
+        if resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
+
+            else:
+                response = f"{get_all_employees()}"
+
+        if resource == "customers":
+            if id is not None:
+                response = f"{get_single_customer(id)}"
+
+            else:
+                response = f"{get_all_customers()}"
 
         # This weird code sends a response back to the client
-        self.wfile.write(f"{response}".encode())
+        self.wfile.write(response.encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new employee
+        new_employee = None
+
+        # Add a new employee to the list. Don't worry about
+        # the orange squiggle, you'll define the create_employee
+        # function next.
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+
+        # Encode the new employee and send in response
+        self.wfile.write(f"{new_employee}".encode())
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new location
+        new_location = None
+
+        # Add a new location to the list. Don't worry about
+        # the orange squiggle, you'll define the create_location
+        # function next.
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+        # Encode the new location and send in response
+        self.wfile.write(f"{new_location}".encode())
+
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new customer
+        new_customer = None
+
+        # Add a new customer to the list. Don't worry about
+        # the orange squiggle, you'll define the create_customer
+        # function next.
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+        # Encode the new customer and send in response
+        self.wfile.write(f"{new_customer}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
@@ -78,7 +208,47 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_PUT(self):
         """Handles PUT requests to the server
         """
-        self.do_POST()
+
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Delete a single location from the list
+        if resource == "locations":
+            delete_location(id)
+
+        # Delete a single employee from the list
+        if resource == "employees":
+            delete_employee(id)
+
+        # Delete a single customer from the list
+        if resource == "customers":
+            delete_customer(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
---
+++ views/employees_requests.py
@@ -0,0 +1,177 @@
+EMPLOYEES = [
+    {
+      "id": 1,
+      "name": "Jessica Younker",
+      "email": "jessica@younker.com",
+      "employee": True
+    },
+    {
+      "id": 2,
+      "name": "Jordan Nelson",
+      "email": "jordan@nelson.com",
+      "employee": True
+    },
+    {
+      "id": 3,
+      "name": "Zoe LeBlanc",
+      "email": "zoe@leblanc.com",
+      "employee": True
+    },
+    {
+      "name": "Meg Ducharme",
+      "email": "meg@ducharme.com",
+      "id": 4,
+      "employee": True
+    },
+    {
+      "name": "Hannah Hall",
+      "email": "hannah@hall.com",
+      "id": 5,
+      "employee": True
+    },
+    {
+      "name": "Emily Lemmon",
+      "email": "emily@lemmon.com",
+      "id": 6,
+      "employee": True
+    },
+    {
+      "name": "Jordan Castelloe",
+      "email": "jordan@castelloe.com",
+      "id": 7,
+      "employee": True
+    },
+    {
+      "name": "Leah Gwin",
+      "email": "leah@gwin.com",
+      "id": 8,
+      "employee": True
+    },
+    {
+      "name": "Caitlin Stein",
+      "email": "caitlin@stein.com",
+      "id": 9,
+      "employee": True
+    },
+    {
+      "name": "Greg Korte",
+      "email": "greg@korte.com",
+      "id": 10,
+      "employee": True
+    },
+    {
+      "name": "Charisse Lambert",
+      "email": "charisse@lambert.com",
+      "id": 11,
+      "employee": True
+    },
+    {
+      "name": "Madi Peper",
+      "email": "madi@peper.com",
+      "id": 12,
+      "employee": True
+    },
+    {
+      "name": "Jenna Solis",
+      "email": "jenna@solis.com",
+      "id": 14,
+      "employee": True
+    },
+    {
+      "id": 15,
+      "name": "Ryan Tanay",
+      "email": "ryan@tanay.com",
+      "employee": False
+    },
+    {
+      "id": 16,
+      "name": "Emma Beaton",
+      "email": "emma@beaton.com",
+      "employee": False
+    },
+    {
+      "id": 17,
+      "name": "Dani Adkins",
+      "email": "dani.adkins.com",
+      "employee": False
+    },
+    {
+      "id": 18,
+      "name": "Adam Oswalt",
+      "email": "adam@oswalt.com",
+      "employee": False
+    },
+    {
+      "id": 19,
+      "name": "Fletcher Bangs",
+      "email": "flangs@bangs.com",
+      "employee": False
+    },
+    {
+      "id": 20,
+      "name": "Angela Lee",
+      "email": "lee@lee.com",
+      "employee": False
+    },
+    {
+      "name": "mike mike",
+      "email": "m@m.com",
+      "employee": False,
+      "id": 21
+    },
+    {
+      "name": "Eric \"Macho Man\" Taylor",
+      "email": "macho@man.com",
+      "employee": True,
+      "id": 22
+    }
+  ]
+
+def get_all_employees():
+    return EMPLOYEES
+
+# Function with a single parameter
+def get_single_employee(id):
+    # Variable to hold the found employee, if it exists
+    requested_employee = None
+
+    # Iterate the EMPLOYEES list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
+
+def create_employee(employee):
+    # Get the id value of the last employee in the list
+    max_id = EMPLOYEES[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the employee dictionary
+    employee["id"] = new_id
+
+    # Add the employee dictionary to the list
+    EMPLOYEES.append(employee)
+
+    # Return the dictionary with `id` property added
+    return employee
+
+def delete_employee(id):
+    # Initial -1 value for employee index, in case one isn't found
+    employee_index = -1
+
+    # Iterate the EMPLOYEES list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            # Found the employee. Store the current index.
+            employee_index = index
+
+    # If the employee was found, use pop(int) to remove it from list
+    if employee_index >= 0:
+        EMPLOYEES.pop(employee_index)
